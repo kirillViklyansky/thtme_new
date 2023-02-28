55cd3d61782177b15588cb58249a22f987f1d8aa
KirillVikliansky <61971879+kirillViklyansky@users.noreply.github.com>
2023-02-28 10:03:47 +0200

    part

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+function include_acf_placeholder() {
+    include_once get_stylesheet_directory() . '/inc/acf-placeholder.php';
+}
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
