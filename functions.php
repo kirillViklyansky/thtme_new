199552db424ef7a47a89e5d53f4e2e1877ee8e07
Rostislav Demenko <rostiquell@gmail.com>
2023-02-28 12:55:26 +0200

    Theme Update

--- functions.php
+++ functions.php
@@ -1,38 +1,28 @@
 <?php
-    function foundation_scripts_and_styles() {
+    function rk_scripts() {
         if ( ! is_admin() ) {
-
             // Disable gutenberg built-in styles
             wp_dequeue_style( 'wp-block-library' );
 
-            // Load Stylesheets
-            //core
+            // Custom CSS
+            wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.min.css', null, null );
+            wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', null, null );
 
-            //system
-            wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css', null, null );/*2rd priority*/
-            wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', null, null );/*1st priority*/
-
-            // Load JavaScripts
-            //core
+            // Load Core JavaScripts
             wp_enqueue_script( 'jquery' );
             wp_enqueue_script( 'jquery-ui-core' );
             wp_enqueue_script( 'jquery-ui-selectmenu' );
 
 
-            //plugins
+            // Plugins
             wp_enqueue_script( 'matchHeight', get_template_directory_uri() . '/assets/js/plugins/jquery.matchHeight-min.js', null, '0.7.2', true );
 
-            //custom javascript
-            wp_enqueue_script( 'global', get_template_directory_uri() . '/assets/js/global.js', null, null, true ); /* This should go first */
-
+            // Custom Javascript
+            wp_enqueue_script( 'global', get_template_directory_uri() . '/assets/js/global.min.js', null, null, true );
         }
     }
 
-
-    function include_acf_placeholder() {
-        include_once get_stylesheet_directory() . '/inc/acf-placeholder.php';
-    }
-
+    add_action( 'wp_enqueue_scripts', 'foundation_scripts_and_styles' );
 
 ?>
 
